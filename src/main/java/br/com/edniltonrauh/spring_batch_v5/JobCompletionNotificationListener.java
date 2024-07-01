0e31e9cbbc748c15548b502e9d64ea4c1dbac73b
Ednilton C. Rauh <edrauh@gmail.com>
2024-07-01 14:40:18 -0300

    Primeiro commit

---
+++ src/main/java/br/com/edniltonrauh/spring_batch_v5/JobCompletionNotificationListener.java
@@ -0,0 +1,34 @@
+package br.com.edniltonrauh.spring_batch_v5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.jdbc.core.DataClassRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener implements JobExecutionListener {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB CONCLUÍDO! Verifique os resultados gerados.");
+
+            jdbcTemplate
+                    .query("SELECT first_name, last_name FROM people", new DataClassRowMapper<>(Person.class))
+                    .forEach(person -> log.info("Found <{{}}> in the database.", person));
+        }
+    }
+}
---
+++ src/main/java/br/com/edniltonrauh/spring_batch_v5/Person.java
@@ -0,0 +1,5 @@
+package br.com.edniltonrauh.spring_batch_v5;
+
+public record Person(String firstName, String lastName) {
+
+}
